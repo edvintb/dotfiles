a77acc05256f36d8ce86e1971b819d0d5bb73ce4
edvintb <edvinb33@gmail.com>
2025-02-10 09:52:36 -0800

    Remove nvim image plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,2 @@
-vim.g.python3_host_prog=vim.fn.expand("~/.virtualenvs/neovim/bin/python3")
 require("config.lazy")
 
--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -76,33 +76,6 @@ require("lazy").setup({
         }
     },
     {
-        "benlubas/molten-nvim",
-        dependencies = { "3rd/image.nvim" },
-        build = ":UpdateRemotePlugins",
-        init = function()
-            -- I find auto open annoying, keep in mind setting this option will require setting
-            -- a keybind for `:noautocmd MoltenEnterOutput` to open the output again
-            vim.g.molten_auto_open_output = false
-
-            -- this guide will be using image.nvim
-            -- Don't forget to setup and install the plugin if you want to view image outputs
-            vim.g.molten_image_provider = "image.nvim"
-
-            -- optional, I like wrapping. works for virt text and the output window
-            vim.g.molten_wrap_output = true
-
-            -- Output as virtual text. Allows outputs to always be shown, works with images, but can
-            -- be buggy with longer images
-            vim.g.molten_virt_text_output = true
-
-            -- this will make it so the output shows up below the \`\`\` cell delimiter
-            vim.g.molten_virt_lines_off_by_1 = true
-
-            vim.keymap.set("n", "<localleader>e", ":MoltenEvaluateOperator<CR>", { desc = "evaluate operator", silent = true })
-            vim.keymap.set("n", "<localleader>os", ":noautocmd MoltenEnterOutput<CR>", { desc = "open output window", silent = true })
-        end,
-    },
-    {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
         dependencies = {
@@ -153,24 +126,11 @@ require("lazy").setup({
         ft = "markdown",
         dependencies = { "nvim-lua/plenary.nvim" },
     },
-    {
-        "3rd/image.nvim",
-        opts = {
-            -- options from the not-so-quick-start guide for the molten plugin
-            backend = "kitty", -- whatever backend you would like to use
-            max_width = 100,
-            max_height = 12,
-            max_height_window_percentage = math.huge,
-            max_width_window_percentage = math.huge,
-            window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
-            window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-        }
-    },
 }, {
     -- options
-    rocks = {
-        hererocks = true,  -- recommended if you do not have global installation of Lua 5.1.
-    },
+    -- rocks = {
+    --     hererocks = true,  -- recommended if you do not have global installation of Lua 5.1.
+    -- },
 
     defaults = {
         lazy = false,
@@ -178,3 +138,43 @@ require("lazy").setup({
     }
 })
 
+--     {
+--         "benlubas/molten-nvim",
+--         dependencies = { "3rd/image.nvim" },
+--         build = ":UpdateRemotePlugins",
+--         init = function()
+--             -- I find auto open annoying, keep in mind setting this option will require setting
+--             -- a keybind for `:noautocmd MoltenEnterOutput` to open the output again
+--             vim.g.molten_auto_open_output = false
+-- 
+--             -- this guide will be using image.nvim
+--             -- Don't forget to setup and install the plugin if you want to view image outputs
+--             vim.g.molten_image_provider = "image.nvim"
+-- 
+--             -- optional, I like wrapping. works for virt text and the output window
+--             vim.g.molten_wrap_output = true
+-- 
+--             -- Output as virtual text. Allows outputs to always be shown, works with images, but can
+--             -- be buggy with longer images
+--             vim.g.molten_virt_text_output = true
+-- 
+--             -- this will make it so the output shows up below the \`\`\` cell delimiter
+--             vim.g.molten_virt_lines_off_by_1 = true
+-- 
+--             vim.keymap.set("n", "<localleader>e", ":MoltenEvaluateOperator<CR>", { desc = "evaluate operator", silent = true })
+--             vim.keymap.set("n", "<localleader>os", ":noautocmd MoltenEnterOutput<CR>", { desc = "open output window", silent = true })
+--         end,
+--     },
+--     {
+--         "3rd/image.nvim",
+--         opts = {
+--             -- options from the not-so-quick-start guide for the molten plugin
+--             backend = "kitty", -- whatever backend you would like to use
+--             max_width = 100,
+--             max_height = 12,
+--             max_height_window_percentage = math.huge,
+--             max_width_window_percentage = math.huge,
+--             window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
+--             window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+--         }
+--     },
