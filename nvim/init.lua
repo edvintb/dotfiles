6731e52fc86d6d19a07f7ab5a89b2637ec9dcf5d
edvintb <edvinb33@gmail.com>
2025-02-19 16:13:27 -0800

    Find zsh executeable using 'which' instead of 'command'

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,7 @@
 require("config.lazy")
 -- Find zsh automatically and make it the default shell:
 if vim.fn.executable("zsh") == 1 then
-  local zsh_path = vim.fn.system({"command", "-v", "zsh"})
+  local zsh_path = vim.fn.system({"which", "zsh"})
   zsh_path = string.gsub(zsh_path, "%s+", "") -- Remove any trailing whitespace
   vim.env.SHELL = zsh_path
 end
