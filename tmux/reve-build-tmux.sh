a0759dabb18eeb80b584c555591c816dfafdad09
edvintb <edvinb33@gmail.com>
2025-04-02 10:33:15 -0700

    Add tmux ubuntu build script

--- tmux/reve-build-tmux.sh
+++ tmux/reve-build-tmux.sh
@@ -1,3 +1,28 @@
 # follow instructions on https://github.com/tmux/tmux/wiki/Installing
 
 # check out the latest release branch after cd into repo: git checkout <latest stable branch name>
+
+# install build packages 
+apt-get install libevent-dev ncurses-dev build-essential bison pkg-config
+
+# install configuration dependencies
+apt-get install autoconf automake
+
+# install run dependencies
+apt-get install libevent ncurses
+
+# clone and install into $HOME
+cd $HOME
+
+# remove existing tmux directory
+rm -rf tmux
+
+git clone https://github.com/tmux/tmux.git
+cd tmux
+sh autogen.sh
+./configure
+make && sudo make install
+
+# source .zshrc again to update $PATH and put tmux there
+cd $HOME
+source .zshrc
